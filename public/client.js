92bdc76a4807d25866b6c6f574168c52e0d9df7f
Glitch (clayton-final-exam) <none>
2020-01-24 15:09:02 +0000

    📒🛍 Updated with Glitch

--- public/client.js
+++ public/client.js
@@ -1,42 +1,215 @@
-// client-side js
-// run by the browser each time your view template is loaded
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+var ballRadius = 20;
+var x = canvas.width / 4;
+var y = canvas.height - 60;
+var dx = 4;
+var dy = -4;
+var paddleHeight = 10;
+var paddleWidth = 150;
+var paddleX = (canvas.width - paddleWidth) / 4;
+var rightPressed = false;
+var leftPressed = false;
+var brickRowCount = 14;
+var brickColumnCount = 8;
+var brickWidth = 75;
+var brickHeight = 20;
+var brickPadding = 10;
+var brickOffsetTop = 30;
+var brickOffsetLeft = 30;
+var score = 0;
+var lives = 5;
+var bricks = [];
 
-console.log("hello world :o");
+for (var c = 0; c < brickColumnCount; c++) {
+  bricks[c] = [];
 
-// our default array of dreams
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
+  for (var r = 0; r < brickRowCount; r++) {
+    bricks[c][r] = { x: 0, y: 0, status: 1 };
+  }
+}
 
-// define variables that reference elements on our page
-const dreamsList = document.getElementById("dreams");
-const dreamsForm = document.forms[0];
-const dreamInput = dreamsForm.elements["dream"];
+document.addEventListener("keydown", keyDownHandler, false);
 
-// a helper function that creates a list item for a given dream
-const appendNewDream = function(dream) {
-  const newListItem = document.createElement("li");
-  newListItem.innerHTML = dream;
-  dreamsList.appendChild(newListItem);
-};
+document.addEventListener("keyup", keyUpHandler, false);
 
-// iterate through every dream and add it to our page
-dreams.forEach(function(dream) {
-  appendNewDream(dream);
-});
+document.addEventListener("mousemove", mouseMoveHandler, false);
 
-// listen for the form to be submitted and add a new dream when it is
-dreamsForm.onsubmit = function(event) {
-  // stop our form submission from refreshing the page
-  event.preventDefault();
+function keyDownHandler(e) {
+  if (e.keyCode == 39) {
+    rightPressed = true;
+  } else if (e.keyCode == 37) {
+    leftPressed = true;
+  }
+}
 
-  // get dream value and add it to the list
-  dreams.push(dreamInput.value);
-  appendNewDream(dreamInput.value);
+function keyUpHandler(e) {
+  if (e.keyCode == 39) {
+    rightPressed = false;
+  } else if (e.keyCode == 37) {
+    leftPressed = false;
+  }
+}
 
-  // reset form
-  dreamInput.value = "";
-  dreamInput.focus();
-};
+function mouseMoveHandler(e) {
+  var relativeX = e.clientX - canvas.offsetLeft;
+
+  if (relativeX > 0 && relativeX < canvas.width) {
+    paddleX = relativeX - paddleWidth / 2;
+  }
+}
+
+function collisionDetection() {
+  for (var c = 0; c < brickColumnCount; c++) {
+    for (var r = 0; r < brickRowCount; r++) {
+      var b = bricks[c][r];
+
+      if (b.status == 1) {
+        if (
+          x > b.x &&
+          x < b.x + brickWidth &&
+          y > b.y &&
+          y < b.y + brickHeight
+        ) {
+          dy = -dy;
+
+          b.status = 0;
+
+          score++;
+
+          if (score == brickRowCount * brickColumnCount) {
+            alert("Congratulations! You beat the game!");
+
+            document.location.reload();
+          }
+        }
+      }
+    }
+  }
+}
+
+function drawBall() {
+  ctx.beginPath();
+
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+
+  ctx.fillStyle = "#8258FA";
+
+  ctx.fill();
+
+  ctx.closePath();
+}
+
+function drawPaddle() {
+  ctx.beginPath();
+
+  ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+
+  ctx.fillStyle = "#3ADF00";
+
+  ctx.fill();
+
+  ctx.closePath();
+}
+
+function drawBricks() {
+  for (var c = 0; c < brickColumnCount; c++) {
+    for (var r = 0; r < brickRowCount; r++) {
+      if (bricks[c][r].status == 1) {
+        var brickX = r * (brickWidth + brickPadding) + brickOffsetLeft;
+
+        var brickY = c * (brickHeight + brickPadding) + brickOffsetTop;
+
+        bricks[c][r].x = brickX;
+
+        bricks[c][r].y = brickY;
+
+        ctx.beginPath();
+
+        ctx.rect(brickX, brickY, brickWidth, brickHeight);
+
+        ctx.fillStyle = "#585858";
+
+        ctx.fill();
+
+        ctx.closePath();
+      }
+    }
+  }
+}
+
+function drawScore() {
+  ctx.font = "20px Cursive";
+
+  ctx.fillStyle = "#D8CEF6";
+
+  ctx.fillText("Points: " + score, 8, 20);
+}
+
+function drawLives() {
+  ctx.font = "20px Cursive";
+
+  ctx.fillStyle = "#D8CEF6";
+
+  ctx.fillText("Lives Left: " + lives, canvas.width - 130, 20);
+}
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  drawBricks();
+
+  drawBall();
+
+  drawPaddle();
+
+  drawScore();
+
+  drawLives();
+
+  collisionDetection();
+
+  if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+    dx = -dx;
+  }
+
+  if (y + dy < ballRadius) {
+    dy = -dy;
+  } else if (y + dy > canvas.height - ballRadius) {
+    if (x > paddleX && x < paddleX + paddleWidth) {
+      dy = -dy;
+    } else {
+      lives--;
+
+      if (!lives) {
+        alert("Darn! You lost :( Try again!");
+
+        document.location.reload();
+      } else {
+        x = canvas.width / 2;
+
+        y = canvas.height - 30;
+
+        dx = 3;
+
+        dy = -3;
+
+        paddleX = (canvas.width - paddleWidth) / 2;
+      }
+    }
+  }
+
+  if (rightPressed && paddleX < canvas.width - paddleWidth) {
+    paddleX += 7;
+  } else if (leftPressed && paddleX > 0) {
+    paddleX -= 7;
+  }
+
+  x += dx;
+
+  y += dy;
+
+  requestAnimationFrame(draw);
+}
+
+draw();
